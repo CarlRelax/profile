91186f8645e6b0b9d3b6b81138c9ae1f284d0c50
carl <370799682@qq.com>
2025-12-25 17:54:29 +0800

    almostly complete personal config in nvim

--- nvim/lua/config/options.lua
+++
@@ -1,7 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
--- proxy
--- vim.env.http_proxy = "http://127.0.0.1:7890"
--- vim.env.https_proxy = "http://127.0.0.1:7890"
--- vim.env.ALL_PROXY = "socks5://127.0.0.1:7891"
