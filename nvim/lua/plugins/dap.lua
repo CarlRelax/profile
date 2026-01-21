3e103210567d6858db4d091453345d33d35a55f9
carl <370799682@qq.com>
2026-01-21 23:56:46 +0800

    Fix Load Order

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -7,6 +7,7 @@ return {
       "theHamsta/nvim-dap-virtual-text",
       "nvim-neotest/nvim-nio",
     },
+    event = "BufReadPost",
     config = function()
       local dap = require("dap")
       local dapui = require("dapui")
@@ -113,6 +114,22 @@ return {
 
       -- C 复用 C++
       dap.configurations.c = dap.configurations.cpp
+
+      ------------------------------------------------------------------
+      -- Keymaps
+      ------------------------------------------------------------------
+      vim.keymap.set("n", "<F5>", dap.continue, { desc = "DAP Continue" })
+      vim.keymap.set("n", "<F10>", dap.step_over, { desc = "DAP Step Over" })
+      vim.keymap.set("n", "<F11>", dap.step_into, { desc = "DAP Step Into" })
+      vim.keymap.set("n", "<F12>", dap.step_out, { desc = "DAP Step Out" })
+
+      vim.keymap.set("n", "<Leader>db", dap.toggle_breakpoint, { desc = "DAP Toggle Breakpoint" })
+      vim.keymap.set("n", "<Leader>dB", function()
+        dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+      end, { desc = "DAP Conditional Breakpoint" })
+
+      vim.keymap.set("n", "<Leader>dr", dap.repl.open, { desc = "DAP REPL" })
+      vim.keymap.set("n", "<Leader>dl", dap.run_last, { desc = "DAP Run Last" })
     end,
   },
 }
