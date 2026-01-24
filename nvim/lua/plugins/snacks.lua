28e06c14c8ae768d53c8dc8c848c847c1eb434f9
carl <370799682@qq.com>
2026-01-24 15:15:00 +0800

    Add Dashboard

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -25,6 +25,13 @@ return {
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
       },
+      sections = {
+        { section = "header" },
+        { icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+        { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { section = "startup" },
+      },
     },
     explorer = { enabled = true },
     indent = { enabled = true },
